085c34a5278d5abd053ddd4ea746b439d6d9a414
Pedro Araujo <pe.araujo21@gmail.com>
2021-02-17 16:26:17 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Excpetions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Excpetions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) :base(message)
+        {
+
+        }
+    }
+}
